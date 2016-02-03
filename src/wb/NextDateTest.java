336c86fabee8277e39128cbecc451aa5c03ade64
Stefan Eng <eng_steff@hotmail.com>
2016-02-03 20:46:11 +0100

    Add TestCase class.

--- src/wb/NextDateTest.java
+++ src/wb/NextDateTest.java
@@ -10,6 +10,18 @@ import org.junit.Test;
 
 public class NextDateTest {
 	
+	public class TestCase{
+		String result;
+		int month;
+		int day;
+		int year;
+		
+		public TestCase(int day, int month, int year, String result) {
+			this.day = day;
+			this.month = month;
+			this.year = year;
+			this.result = result;
+		}
 	}
 
 	NextDate next_date = null;
