6474a4d6c0094d1171031cf5bd7586a0df0175b9
Stefan Eng <eng_steff@hotmail.com>
2016-02-03 20:45:58 +0100

    Remove comments.

--- src/wb/NextDateTest.java
+++ src/wb/NextDateTest.java
@@ -8,43 +8,18 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-/**
- * TriangleTest for testing the Triangle class.
- * This template is used in the exercise phase 1.
- * Students should add relevant unit test cases related to the Triangle 
- * class to this class.
- */
 public class NextDateTest {
+	
+	}
 
 	NextDate next_date = null;
-	
-	@BeforeClass
-	/*
-	 * The method run once before any of the test methods in the class.
-	 */
-	public static void setUpBeforeClass() throws Exception {
-	
-	}
-
-	@AfterClass
-	/* 
-	 * The method will be run after all the tests in the class have been run
-	 */
-	public static void tearDownAfterClass() throws Exception {
-	}
 
 	@Before
-	/*
-	 * Initializes common objects. The method will be run before the Test method.
-	 */
 	public void setUp() throws Exception {
 		next_date = new NextDate(0,0,0);
 	}
 
 	@After
-	/*
-	 *   Cleanup method. This method will be run after the Test method is completed
-	 */
 	public void tearDown() throws Exception {
 		next_date = null;
 	}
@@ -53,5 +28,4 @@ public class NextDateTest {
 	@Test
 	public void test() {
 	}
-
 }
